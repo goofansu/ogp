4a1f653330f5ceb09875f41e9c65dfad661f867c
Yejun Su <yejun@hey.com>
2021-05-28 19:25:20 +0800

    add CI

--- mix.exs
+++ mix.exs
@@ -5,7 +5,7 @@ defmodule OpenGraph.MixProject do
     [
       app: :ogp,
       version: "1.0.0",
-      elixir: "~> 1.10",
+      elixir: "~> 1.7",
       start_permanent: Mix.env() == :prod,
       deps: deps()
     ]
