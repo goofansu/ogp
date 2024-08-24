512a6194e7b21c391c84248eb0a4446a01ded2a2
Yejun Su <goofan.su@gmail.com>
2024-08-25 02:05:33 +0800

    bump 1.1.0

--- mix.exs
+++ mix.exs
@@ -4,7 +4,7 @@ defmodule OpenGraph.MixProject do
   def project do
     [
       app: :ogp,
-      version: "1.0.2",
+      version: "1.1.0",
       elixir: "~> 1.13",
       start_permanent: Mix.env() == :prod,
       deps: deps(),
