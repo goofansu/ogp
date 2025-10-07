24e864ed39c191cf98864a604f2b629e1997f2be
Yejun Su <goofan.su@gmail.com>
2025-10-07 23:49:22 +0800

    add castore for test and loose plug ~> 1.0

--- mix.exs
+++ mix.exs
@@ -46,9 +46,10 @@ defmodule OpenGraph.MixProject do
     [
       {:req, "~> 0.5"},
       {:floki, "~> 0.35"},
-      {:plug, "~> 1.16", only: :test},
+      {:plug, "~> 1.0", only: :test},
+      {:ex_doc, ">= 0.0.0", only: :dev, runtime: false},
       {:excoveralls, "~> 0.18.2", only: :test},
-      {:ex_doc, ">= 0.0.0", only: :dev, runtime: false}
+      {:castore, "~> 1.0", only: :test}
     ]
   end
 end
